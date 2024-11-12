977c200ab3c2527063de4889bdf5a743bb85a554
NassarHQ <mnassar.personal@gmail.com>
2024-11-11 17:27:06 -0800

    Added SnakeGameBoard.java for the snake game board

---
+++ src/main/java/com/cs151/blackjackproject/SnakeGameBoard.java
@@ -0,0 +1,9 @@
+package com.cs151.blackjackproject;
+
+/*
+ * SnakeGameBoard is specificly made for snake game.
+ * It contains the dimenstions and limitations of snake movement.
+ */
+public class SnakeGameBoard {
+
+}
