30af8e19572673e25c85ebd8504b31568a31c305
NassarHQ <mnassar.personal@gmail.com>
2024-11-11 16:28:59 -0800

    Organized project structure with package declaration

---
+++ src/main/java/com/cs151/blackjackproject/App.java
@@ -0,0 +1,11 @@
+package com.cs151.blackjackproject;
+
+import java.util.logging.Logger;
+
+public class App {
+    private static final Logger LOGGER = Logger.getLogger(App.class.getName());
+
+    public static void main(String[] args) {
+        LOGGER.info("Hello, BlackJack Project!");
+    }
+}
---
+++ src/main/java/com/cs151/blackjackproject/GameController.java
@@ -0,0 +1,8 @@
+package com.cs151.blackjackproject;
+
+public interface GameController {
+    void startGame();
+    void endGame();
+    void pauseGame();
+    void update();
+}
