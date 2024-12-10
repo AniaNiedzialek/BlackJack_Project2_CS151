f48f5233963c81415290b1f6737209c92994ea5c
NassarHQ <mnassar.personal@gmail.com>
2024-12-09 18:23:02 -0800

    WORKING ON SNAKE POM

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,15 @@
-module com.game {
-    requires transitive javafx.controls;
-    requires javafx.graphics;
-
-    exports com.game;
-}
-
 // module com.game {
-//    requires javafx.controls;
-//    requires javafx.graphics;
-//    exports com.game.Snake to javafx.graphics;
+//     requires transitive javafx.controls;
+//     requires javafx.graphics;
+
+//     exports com.game;
 // }
+
+module com.game {
+   requires javafx.controls;
+   requires javafx.graphics;
+   exports com.game.Snake to javafx.graphics;
+}
 
 // module com.game {
 //    requires javafx.controls;
